7bcac835b7b8bf4f40647dbf258203e3db5fdb8d
kimamovic21 <imamovic.kerim@gmail.com>
2024-06-21 23:36:41 +0200

    Section 2: Next.js Fundamentals & Project Start
    -preuzet starter folder _theme_files
    -u folderu app kreirane jsx komponente
    -u folderu app kreirani folder properties, add i [id]
    -kreiran folder components i jsx komponente
    -instalirani paketi u terminalu
    -u folderu public kreiran folder images sa slikama
    -kreiran properties.json fajl
    -kreiran folderi assets i unutar njega folderi images i styles
    -manje izmjene u fajlovima README.md i documentation.txt

---
+++ app/layout.jsx
@@ -0,0 +1,25 @@
+import '@/assets/styles/globals.css';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+
+export const metadata = {
+  title: 'PropertyPulse | Find The Perfect Rental',
+  description: 'Find your dream rental property',
+  keywords: 'rental, find rentals, find properties',
+};
+
+const MainLayout = ({ children }) => {
+  return (
+    <html lang='en'>
+        <body>
+            <Navbar />
+            <main>
+                {children}
+            </main>
+            <Footer />
+        </body>
+    </html>
+  );
+};
+
+export default MainLayout;
